9d66c01fd9c2c0570afd9c48167eb32247831b8e
RuolinLi06 <li.ruolin@northeastern.edu>
2023-11-25 20:32:22 -0500

    build kafka consumer and es

---
+++ es_consumer/src/main/java/com/neu/bigdata/es_consumer/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.neu.bigdata.es_consumer.constant;
+
+/**
+ * @author Ruolin Li
+ * @Date 2023-11-25
+ */
+public class Constant {
+    public static String PLAN = "plan";
+
+    public static String TYPE = "_doc";
+
+    public static String ES_POST = "index";
+
+    public static String ES_DELETE = "delete";
+}
---
+++ es_consumer/src/main/java/com/neu/bigdata/es_consumer/listener/KafkaConsumer.java
@@ -0,0 +1,50 @@
+package com.neu.bigdata.es_consumer.listener;
+
+import com.neu.bigdata.es_consumer.constant.Constant;
+import com.neu.bigdata.es_consumer.dao.ElasticSearchDao;
+import org.springframework.stereotype.Service;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import java.io.IOException;
+/**
+ * @author Ruolin Li
+ * @Date 2023-11-25
+ */
+@Service
+public class KafkaConsumer {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    ElasticSearchDao elasticSearchDao;
+
+    @KafkaListener(topics = "info7255")
+    public void OpsSub(ConsumerRecord<String, String> record) {
+        logger.info("Consumed Message - {} ", record);
+        logger.info("Consumed message: \nKey: " + record.key().toString() + " \nValue: " + record.value().toString());
+
+        // Send Message to elastic search
+        if (record.key().toString().equals(Constant.ES_POST)) {
+            logger.info("CONSUMER " + record.key() + " : " + record.value());
+
+            JSONObject planJson = new JSONObject(record.value().toString());
+
+            try {
+                elasticSearchDao.postDocument(planJson);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else if (record.key().toString().equals(Constant.ES_DELETE)) {
+            logger.info("CONSUMER " + record.key() + " : " + record.value());
+
+            String objId = record.value().toString();
+
+            elasticSearchDao.deletePlanDoc(objId);
+        }
+    }
+
+}
