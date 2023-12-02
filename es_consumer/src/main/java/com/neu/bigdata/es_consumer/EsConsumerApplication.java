7f2141a606aec9abc40fcc23096c8c23a281dc4e
RuolinLi06 <li.ruolin@northeastern.edu>
2023-12-02 09:42:20 -0500

    update

---
+++ es_consumer/src/main/java/com/neu/bigdata/es_consumer/EsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.neu.bigdata.es_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsConsumerApplication.class, args);
+	}
+
+}
---
+++ es_consumer/src/test/java/com/neu/bigdata/es_consumer/EsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.bigdata.es_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
